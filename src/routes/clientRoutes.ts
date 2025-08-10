21a70a4253eb7db57b9adc0d7b66877cb0c3d939
Luis Henrique <luishsilva09@gmail.com>
2025-08-10 20:24:13 -0300

    Add autenticação nas rotas

--- src/routes/clientRoutes.ts
+++ src/routes/clientRoutes.ts
@@ -3,9 +3,24 @@ import { createClient, deleteClient, listClient, updateClient, listClientById } 
 import { createClientJsonSchema, updateClientJsonSchema } from '../schemas/clientSchema'
 
 export async function clientRoutes(fastify: FastifyInstance) {
-    fastify.post('/client', { schema: createClientJsonSchema }, createClient)
-    fastify.get('/client',listClient)
-    fastify.get('/client', listClientById)
-    fastify.put('/client/:id', { schema: updateClientJsonSchema }, updateClient)
-    fastify.delete('/client/:id', deleteClient)
-}
+    fastify.post('/client', { 
+        schema: createClientJsonSchema,
+        preHandler: [fastify.authenticate]
+    }, createClient)
+    fastify.get('/client', { preHandler: [fastify.authenticate] }, listClient)
+    fastify.get('/client/:id', { preHandler: [fastify.authenticate] }, listClientById)
+    fastify.put(
+        '/client/:id',
+        {
+            schema: updateClientJsonSchema,
+            preHandler: [fastify.authenticate, fastify.authorize(['ADVISOR'])],
+        },
+        updateClient
+    )
+    fastify.delete(
+        '/client/:id',
+        {
+            preHandler: [fastify.authenticate, fastify.authorize(['ADVISOR'])],
+        },
+        deleteClient
+    ) }
